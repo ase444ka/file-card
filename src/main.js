44c8452ed398dd861c02fe2a0d08945c0eff75a3
Ase444ka <istomina.asia@yandex.ru>
2025-03-19 16:37:49 +0300

    add web-component

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import '@/assets/style.css';
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
 import {setupCounter} from './counter.js';
+import FileCard from '@/FileCard.js'
 
 document.querySelector('#app').innerHTML = /*jsx*/ `
-  <div></div>
+  <file-card></file-card>
 `;
 
 setupCounter(document.querySelector('#counter'));
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import url from 'url'
+import {resolve} from 'path'
+
 import {defineConfig} from 'vite'
+
 import postcssNesting from 'postcss-nested'
-// import postcssMixins from 'postcss-mixins'
+import cssnanoPlugin from 'cssnano'
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': url.fileURLToPath(new URL('./src', import.meta.url)),
+      '@':  url.fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist'),
   },
   css: {
     postcss: {
       plugins: [
         postcssNesting,
+        cssnanoPlugin,
       ],
     },
   },
